a13422c822b16b5559af839ff8d352815da3e33f
Rugger11 <82486396+zimmermanc6588@users.noreply.github.com>
2021-09-18 12:52:33 -0500

    setup class and ran first test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,4 +1,6 @@
 const Manager = require('../lib/Manager.js');
 
 test('creates a manager object', () => {
-    const manager = new Manager();
+    const manager = new Manager( `Sal`, 45, `test@gmail.com`, 12345655555);
+    expect (typeof(manager)).toBe("object")   
+ })
